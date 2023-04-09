aa27e262d62b4df004df28dc2bff11c1d48366d2
ali <farhan8738900076@gmail.com>
2023-04-09 15:28:08 +0530

    added layouts, partials, static files

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const port = 8001;
 const app = express();
+//use express layout
+const expressLayout = require('express-ejs-layouts');
+app.use(expressLayout);
+
+//use static folder
+app.use(express.static('./assets'));
+
+//extract style and script from sub pages into layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 //any request will route through routes folder
 app.use('/', require('./routes'));
 app.set('view engine', 'ejs');
